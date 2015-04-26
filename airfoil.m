af1a66377e0e4343db336ac252546f1bd9c10509
lindsey-andrade <lindsey.andrade@students.olin.edu>
2015-04-25 23:21:00 -0400

    Notation

--- airfoil.m
+++ airfoil.m
@@ -1,8 +1,9 @@
-%% MechSolids Airfoil Project
+% MechSolids Airfoil Project
 % Lindsey Andrade, Ambika Goel, Caleb Kissel
 
 c = 1; %cord length meters
 
+% Constants 
 rho =  1.225; % density of air kg/m3
 V = 200; % airspeed m/s
 F = 100; % Force on wing N
@@ -12,3 +13,6 @@ alpha = ; % angle of attack
 l = F*cos(alpha); %lift
 
 C_lift = l/(.5 * rho * V^2 * c); %coefficient of lift
+
+% Generalization equations 
+
