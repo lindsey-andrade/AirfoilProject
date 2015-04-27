c697853bec0516d5f5a112e53adb21a8f166e1f9
lindsey-andrade <lindsey.andrade@students.olin.edu>
2015-04-27 13:36:09 -0400

    Added real lift force

--- airfoil.m
+++ airfoil.m
@@ -1,18 +1,32 @@
 % MechSolids Airfoil Project
 % Lindsey Andrade, Ambika Goel, Caleb Kissel
 
+function airfoil()
+
 c = 1; %cord length meters
 
 % Constants 
 rho =  1.225; % density of air kg/m3
 V = 200; % airspeed m/s
-F = 100; % Force on wing N
+A = ; % cross section of the wing (for lift)
 
-alpha = ; % angle of attack
+alpha_initial = .1; %initial angle of attack in radians
 
-l = F*cos(alpha); %lift
+LiftForce = 2*pi*(1/2 * rho * V^2 * A); %lift Lift = 2pi(1/2 * rho * V^2 * A); 
 
-C_lift = l/(.5 * rho * V^2 * c); %coefficient of lift
+C_lift = LiftForce/(.5 * rho * V^2 * c); %coefficient of lift
 
-% Generalization equations 
+%     function res = integralFunction(phi)
+%         T = C_lift * (alpha_initial + phi);
+%         res = T/(G*J);
+%     end
+% 
+%     function PHI = fsolveFunction(x)
+% 
+
+
+end
+
+
+
 
