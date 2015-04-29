72ad8690e3ea71652b083d48eac97b9b653404c7
lindsey-andrade <lindsey.andrade@students.olin.edu>
2015-04-29 11:45:53 -0400

    Yay math

--- airfoil.m
+++ airfoil.m
@@ -2,30 +2,38 @@
 % Lindsey Andrade, Ambika Goel, Caleb Kissel
 
 function airfoil()
-
+close all
 c = 1; %cord length meters
 
 % Constants 
 rho =  1.225; % density of air kg/m3
 V = 200; % airspeed m/s
-A = ; % cross section of the wing (for lift)
+A = 8; % cross section of the wing (for lift)
+G = 24e9; %N/m^2 
+J = 1300;
+L = 100; % length of wing (m)
 
 alpha_initial = .1; %initial angle of attack in radians
 
 LiftForce = 2*pi*(1/2 * rho * V^2 * A); %lift Lift = 2pi(1/2 * rho * V^2 * A); 
 
-C_lift = LiftForce/(.5 * rho * V^2 * c); %coefficient of lift
+C = LiftForce/(.5 * rho * V^2 * c); %coefficient of lift
 
-%     function res = integralFunction(phi)
-%         T = C_lift * (alpha_initial + phi);
-%         res = T/(G*J);
-%     end
-% 
-%     function PHI = fsolveFunction(x)
-% 
+X = linspace(0, L, 50);
+T = zeros(1, length(X));
+Phi = zeros(1, length(X));
 
-
+for i = 1 : length(X)
+T(i) = C * alpha_initial * exp((C*X(i))/(J*G)); 
+Phi(i) = alpha_initial * (exp(C*X(i)/(J*G)) - 1); 
 end
+
+plot(X, T)
+
+figure
+plot(X, Phi)
+end
+
 
 
 
